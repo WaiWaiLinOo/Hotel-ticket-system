b85ad812ec1e72a1af2e0335e82ba2a60c3b93fd
Wai Lin Oo <wailinoo.august@gmail.com>
2023-12-01 11:50:20 +0630

    [wlo] update

--- src/controller/hotel.controller.js
+++ src/controller/hotel.controller.js
@@ -1,11 +1,16 @@
 const { getAll } = require('../db/hotel');
+const response = require('../config/response')
 
 exports.getAllController = async (req, res) => {
     try {
         const data = await getAll();
-        res.json(data);
+        res.json(response({
+            success: true,
+            message: "Success!",
+            payload: data
+        }));
     } catch (error) {
         console.error('Error in testController:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ success: false, message: error });
     }
 };
